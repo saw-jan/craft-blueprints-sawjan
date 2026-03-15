65bf47a7cbc9147bdd25e78198dc82d4fbb8e864
Sajan Gurung <saw.jan.grg3e@gmail.com>
2026-03-15 10:34:22 +0545

    fix: add missing kwayland dependency
    Signed-off-by: Sajan Gurung <saw.jan.grg3e@gmail.com>

--- webdriver-atspi/webdriver-atspi.py
+++ webdriver-atspi/webdriver-atspi.py
@@ -6,7 +6,7 @@ class subinfo(info.infoclass):
         self.svnTargets["master"] = "https://invent.kde.org/sdk/selenium-webdriver-at-spi.git|master"
         self.defaultTarget = "master"
         self.description = "WebDriver for AT-SPI"
-        self.displayName = "WebDriver AT-SPI "
+        self.displayName = "WebDriver AT-SPI"
         self.webpage = "https://invent.kde.org/sdk/selenium-webdriver-at-spi"
 
     def setDependencies(self):
@@ -15,10 +15,8 @@ class subinfo(info.infoclass):
 
         self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kwindowsystem"] = None
+        self.runtimeDependencies["kde/plasma/kwayland"] = None
         self.runtimeDependencies["libs/qt/qtbase"] = None
-        self.runtimeDependencies["libs/qt/qttranslations"] = None
-        self.runtimeDependencies["libs/qt/qtsvg"] = None
-        self.runtimeDependencies["libs/qt/qtimageformats"] = None
         self.runtimeDependencies["libs/qt/qtdeclarative"] = None
         self.runtimeDependencies["libs/qt/qtwayland"] = None
 
