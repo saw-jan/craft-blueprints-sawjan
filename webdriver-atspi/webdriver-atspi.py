929df30fe06ca6ed17334d3d7f72ce4ea7c6e41c
Sajan Gurung <saw.jan.grg3e@gmail.com>
2026-03-15 11:47:53 +0545

    feat: add multiple targets
    Signed-off-by: Sajan Gurung <saw.jan.grg3e@gmail.com>

--- webdriver-atspi/webdriver-atspi.py
+++ webdriver-atspi/webdriver-atspi.py
@@ -3,7 +3,8 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.svnTargets["master"] = "https://invent.kde.org/sdk/selenium-webdriver-at-spi.git|master"
+        for ver in ["master", "Plasma/6.6"]:
+            self.svnTargets[ver] = f"https://invent.kde.org/sdk/selenium-webdriver-at-spi.git|{ver}"
         self.defaultTarget = "master"
         self.description = "WebDriver for AT-SPI"
         self.displayName = "WebDriver AT-SPI"
@@ -15,10 +16,11 @@ class subinfo(info.infoclass):
 
         self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kwindowsystem"] = None
-        self.runtimeDependencies["kde/plasma/kwayland"] = None
+        # self.runtimeDependencies["kde/libs/plasma-wayland-protocols"] = None
+        # self.runtimeDependencies["kde/plasma/kwayland"] = None
         self.runtimeDependencies["libs/qt/qtbase"] = None
         self.runtimeDependencies["libs/qt/qtdeclarative"] = None
-        self.runtimeDependencies["libs/qt/qtwayland"] = None
+        # self.runtimeDependencies["libs/qt/qtwayland"] = None
 
 
 from Package.CMakePackageBase import *
