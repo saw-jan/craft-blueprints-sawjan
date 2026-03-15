d26b10852091cf8ed72b2ef3395df4b350e25699
Sajan Gurung <saw.jan.grg3e@gmail.com>
2026-03-15 10:08:35 +0545

    fix: correct kde package path
    Signed-off-by: Sajan Gurung <saw.jan.grg3e@gmail.com>

--- webdriver-atspi/webdriver-atspi.py
+++ webdriver-atspi/webdriver-atspi.py
@@ -5,17 +5,16 @@ class subinfo(info.infoclass):
     def setTargets(self):
         self.svnTargets["master"] = "https://invent.kde.org/sdk/selenium-webdriver-at-spi.git|master"
         self.defaultTarget = "master"
-
-        self.description = "Webdriver for AT-SPI"
-        self.displayName = "AT-SPI Webdriver"
+        self.description = "WebDriver for AT-SPI"
+        self.displayName = "WebDriver AT-SPI "
         self.webpage = "https://invent.kde.org/sdk/selenium-webdriver-at-spi"
 
     def setDependencies(self):
         self.buildDependencies["dev-utils/cmake"] = None
         self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
 
-        self.runtimeDependencies["kde/tier1/kcoreaddons"] = None
-        self.runtimeDependencies["kde/tier1/kwindowsystem"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kwindowsystem"] = None
         self.runtimeDependencies["libs/qt/qtbase"] = None
         self.runtimeDependencies["libs/qt/qttranslations"] = None
         self.runtimeDependencies["libs/qt/qtsvg"] = None
