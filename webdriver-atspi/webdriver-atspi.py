8ab48ad9ce2e7361c97812ec117855a5ad841a2a
Saw-jan <saw.jan.grg3e@gmail.com>
2026-03-16 18:24:07 +0545

    fix: add necessary packages
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>

--- webdriver-atspi/webdriver-atspi.py
+++ webdriver-atspi/webdriver-atspi.py
@@ -17,10 +17,10 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kwindowsystem"] = None
         # self.runtimeDependencies["kde/libs/plasma-wayland-protocols"] = None
-        # self.runtimeDependencies["kde/plasma/kwayland"] = None
+        self.runtimeDependencies["kde/plasma/kwayland"] = None
         self.runtimeDependencies["libs/qt/qtbase"] = None
         self.runtimeDependencies["libs/qt/qtdeclarative"] = None
-        # self.runtimeDependencies["libs/qt/qtwayland"] = None
+        self.runtimeDependencies["libs/qt/qtwayland"] = None
 
 
 from Package.CMakePackageBase import *
